dbaf3471cf4afe0323d625b39de541d218964841
AnnabelleCat <pikainka@gmail.com>
2018-07-10 20:31:58 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -36,5 +36,8 @@ end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  STUDENT_NAMES.firstthree
+  firstthree =  [
+    STUDENT_NAMES[0], STUDENT_NAMES[1], STUDENT_NAMES[2]
+  ] 
+  firstthree
 end
